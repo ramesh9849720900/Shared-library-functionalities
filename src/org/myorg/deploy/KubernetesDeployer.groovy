94a09c3e3abca02a260829c76d261c809a90c6d2
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-23 22:37:15 +0530

    Update KubernetesDeployer.groovy

--- src/org/myorg/deploy/KubernetesDeployer.groovy
+++ src/org/myorg/deploy/KubernetesDeployer.groovy
@@ -1,1 +1,16 @@
-#Write code here...
+package org.myorg.deploy
+
+import static logger.*
+
+class KubernetesDeployer {
+    def deploy(Map config) {
+        try {
+            logInfo("Deploying to Kubernetes: ${config.clusterName}")
+            sh "kubectl config use-context ${config.context}"
+            sh "kubectl apply -f ${config.manifest}"
+        } catch (e) {
+            logError("Kubernetes deployment failed: ${e.message}")
+            throw e
+        }
+    }
+}
