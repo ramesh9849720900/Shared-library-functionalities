78a0b2d8c03ec39cb9b85757d5e7dadc40427d75
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-23 22:37:56 +0530

    Update RetryHelper.groovy

--- src/org/myorg/utils/RetryHelper.groovy
+++ src/org/myorg/utils/RetryHelper.groovy
@@ -1,1 +1,17 @@
-#Write code here...
+package org.myorg.utils
+
+class RetryHelper {
+    static void retry(int times, Closure action) {
+        int attempt = 0
+        while (attempt < times) {
+            try {
+                action.call()
+                return
+            } catch (e) {
+                attempt++
+                if (attempt == times) throw e
+                sleep 2
+            }
+        }
+    }
+}
