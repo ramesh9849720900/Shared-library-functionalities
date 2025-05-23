1788ecb479d41f3f899a2faa69bf59db87f89d3b
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-23 22:34:46 +0530

    Update deployPipeline.groovy

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,1 +1,35 @@
-#Write code here...
+
+@Library('my-shared-lib') _
+
+import org.myorg.config.ConfigLoader
+import org.myorg.deploy.*
+import org.myorg.utils.RetryHelper
+import static logger.*
+
+def call(Map params) {
+    node {
+        def config
+
+        stage('Load Config') {
+            try {
+                config = ConfigLoader.load(params.team)
+                logInfo("Config loaded for ${params.team}")
+            } 
+            catch (e) {
+                logError("Config loading failed: ${e.message}")
+                error("Stopping pipeline due to config error.")
+            }
+        }
+
+        stage('Deploy') {
+            RetryHelper.retry(3) {
+                if (params.deploymentType == 'kubernetes') {
+                    new KubernetesDeployer().deploy(config)
+                } 
+                else {
+                    error("Unsupported deployment type: ${params.deploymentType}")
+                }
+            }
+        }
+    }
+}
