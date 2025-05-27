68ff7773babfae3d0d1b885d2a42f90c98df526e
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-27 10:16:47 +0530

    Update ConfigLoader.groovy

--- src/org/myorg/config/ConfigLoader.groovy
+++ src/org/myorg/config/ConfigLoader.groovy
@@ -1,10 +1,10 @@
 package org.myorg.config
 
-import org.yaml.snakeyaml.Yaml
+import groovy.yaml.YamlSlurper
 
 class ConfigLoader {
     static Map load(String team) {
-        def yamlContent = libraryResource("config/${team}-config.yaml")
-        return new Yaml().load(yamlContent)
+        def file = libraryResource("config/${team}-config.yaml")
+        return new YamlSlurper().parseText(file)
     }
 }
