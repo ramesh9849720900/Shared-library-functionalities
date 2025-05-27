8dafa6a5f2db213ff4e507934db24b0ea06be10c
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-27 10:24:46 +0530

    Update deployPipeline.groovy

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,14 +1,12 @@
-
 @Library('my-shared-lib') _
 
 import org.myorg.config.ConfigLoader
 import org.myorg.deploy.*
 import org.myorg.utils.RetryHelper
-//import static logger.*
-//import vars.Logger
 
 def call(Map params) {
     logger.logInfo("Starting deployment...")
+
     node {
         def config
 
@@ -17,13 +15,13 @@ def call(Map params) {
                 config = ConfigLoader.load(params.team)
                 logger.logInfo("Config loaded for ${params.team}")
                 logger.logInfo("Received config: ${config}")
-            } 
-            catch (e) {
-                if (!config?.env || !config?.namespace || !config?.manifestPath) {
-                    logger.logError("Kubernetes deployment failed")
-                    error("Stopping pipeline due to config error.")
-                }
-                logger.logError("Kuberenetes deployment failed")
+            } catch (e) {
+                logger.logError("Exception while loading config: ${e}")
+                error("Stopping pipeline due to config error.")
+            }
+
+            if (!config?.env || !config?.namespace || !config?.manifestPath) {
+                logger.logError("Config is missing required fields: ${config}")
                 error("Stopping pipeline due to config error.")
             }
         }
@@ -32,8 +30,7 @@ def call(Map params) {
             RetryHelper.retry(3) {
                 if (params.deploymentType == 'kubernetes') {
                     new KubernetesDeployer().deploy(config)
-                } 
-                else {
+                } else {
                     error("Unsupported deployment type: ${params.deploymentType}")
                 }
             }
