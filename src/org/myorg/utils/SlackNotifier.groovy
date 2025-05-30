9b4b4f16dcc546f2c234aa59ce5318097a8c5ec4
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-30 21:03:21 +0530

    Update SlackNotifier.groovy

--- src/org/myorg/utils/SlackNotifier.groovy
+++ src/org/myorg/utils/SlackNotifier.groovy
@@ -33,7 +33,7 @@ _Time_: ${timestamp}
     void notifyFailure(String message) {
         script.slackSend(
         color: 'danger',
-_Triggered by_: ${steps.currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userName ?: "Unknown"}
+echo "Triggered by: ${steps.currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userName ?: "Unknown"}"
 _Time_: ${new Date().format("yyyy-MM-dd HH:mm:ss")}
 """,
         message: ":x: ${message}"
