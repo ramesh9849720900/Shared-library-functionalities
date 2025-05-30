15ce73eef8df7a9fb009cd6b3370a33687992e78
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-30 20:57:04 +0530

    Update SlackNotifier.groovy

--- src/org/myorg/utils/SlackNotifier.groovy
+++ src/org/myorg/utils/SlackNotifier.groovy
@@ -8,10 +8,18 @@ class SlackNotifier implements Serializable {
     }
 
     void send(String message, String color = '#439FE0') {
+         def buildUser = steps.currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userName ?: "Unknown"
+         def timestamp = new Date().format("yyyy-MM-dd HH:mm:ss")
+         def finalMessage = """\
+*${message}*
+_Triggered by_: ${buildUser}
+_Time_: ${timestamp}
+"""
         script.slackSend(
             channel: '#ci-cd-alerts',      // Or make it dynamic
             color: '#439FE0',
             message: message
+            teamDomain: "DevOps_Engineers"
         )
     }
 
@@ -25,7 +33,11 @@ class SlackNotifier implements Serializable {
     void notifyFailure(String message) {
         script.slackSend(
         color: 'danger',
+_Triggered by_: ${steps.currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userName ?: "Unknown"}
+_Time_: ${new Date().format("yyyy-MM-dd HH:mm:ss")}
+""",
         message: ":x: ${message}"
+        teamDomain: "DevOps_Engineers"
         )
     }
 }
