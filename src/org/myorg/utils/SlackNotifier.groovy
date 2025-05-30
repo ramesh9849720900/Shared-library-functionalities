ee39f65a8e75e3706d832d548907f43476ad7135
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-30 21:16:39 +0530

    Update SlackNotifier.groovy

--- src/org/myorg/utils/SlackNotifier.groovy
+++ src/org/myorg/utils/SlackNotifier.groovy
@@ -2,42 +2,51 @@ package org.myorg.utils
 
 class SlackNotifier implements Serializable {
     def script
-
     SlackNotifier(script) {
         this.script = script
     }
 
     void send(String message, String color = '#439FE0') {
-         def buildUser = steps.currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userName ?: "Unknown"
-         def timestamp = new Date().format("yyyy-MM-dd HH:mm:ss")
-         def finalMessage = """\
+        def buildUser = script.currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userName ?: "Unknown"
+        def timestamp = new Date().format("yyyy-MM-dd HH:mm:ss")
+        def finalMessage = """\
 *${message}*
 _Triggered by_: ${buildUser}
 _Time_: ${timestamp}
 """
         script.slackSend(
-            channel: '#ci-cd-alerts',      // Or make it dynamic
-            color: '#439FE0',
-            message: message,
+            channel: '#ci-cd-alerts',
+            color: color,
+            message: finalMessage,
             teamDomain: "DevOps_Engineers"
         )
     }
 
     void notifySuccess(String message) {
         script.slackSend(
-        color: 'good',
-        message: ":white_check_mark: ${message}"
+            color: 'good',
+            message: ":white_check_mark: ${message}",
+            teamDomain: "DevOps_Engineers"
         )
     }
 
     void notifyFailure(String message) {
+        def buildUser = script.currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userName ?: "Unknown"
+        def timestamp = new Date().format("yyyy-MM-dd HH:mm:ss")
+
+        script.echo("Triggered by: ${buildUser}")
+        script.echo("Time: ${timestamp}")
+
+        def finalMessage = """\
+:x: ${message}
+_Triggered by_: ${buildUser}
+_Time_: ${timestamp}
+"""
+
         script.slackSend(
-        color: 'danger',
-steps.echo "Triggered by: ${steps.currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userName ?: 'Unknown'}"
-_Time_: ${new Date().format("yyyy-MM-dd HH:mm:ss")}
-""",
-        message: ":x: ${message}"
-        teamDomain: "DevOps_Engineers"
+            color: 'danger',
+            message: finalMessage,
+            teamDomain: "DevOps_Engineers"
         )
     }
 }
